339651b3ece57cfb84f910c04288d80cf6f5994d
egon w. stemle <egon.stemle@eurac.edu>
2018-03-15 23:53:59 +0100

    Use filtered stop words for training

--- run.py
+++ run.py
@@ -111,11 +111,17 @@ class Corpus():
             csvreader = csv.reader(csvfile, delimiter=self.tokens_delimiter,
                                    quotechar=self.tokens_quotechar)
             for row in csvreader:
+                txt_id, sentence_id, token_id = row[0].split('_')
+
                 if self.mode == "train":
                     label = int(row[1])
+                    # ?FIXME:
+                    # here, we are using the additional information from the
+                    # filtered stop words. still, we are only using the test
+                    # partition so, all in all we should be on the safe side.
+                    label = self.vuamc[txt_id][sentence_id]['labels'][int(token_id)]
                 else:
                     label = -1
-                txt_id, sentence_id, token_id = row[0].split('_')
 
                 if txt_id not in data:
                     data[txt_id] = OrderedDict()
